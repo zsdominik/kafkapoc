c0fc74662e73f28ac225f4d8b9305ce69e5ae299
Dominik Zsiros <Dominik_Zsiros@epam.com>
2019-11-11 14:10:31 +0100

    init

---
+++ src/main/java/com/zsirosd/kafakapoc/KafakapocApplication.java
@@ -0,0 +1,13 @@
+package com.zsirosd.kafakapoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafakapocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafakapocApplication.class, args);
+	}
+
+}
